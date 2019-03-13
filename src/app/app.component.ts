ce820fafdc51b6bea5d5403f7a934cbd4480a5d5
Manishthakur1297 <Manishthakur1297@gmail.com>
2019-03-13 09:23:34 +0530

    intial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'formsDriven';
+export class AppComponent implements OnInit{
+
+  form;
+
+  ngOnInit(){
+    this.form = new FormGroup
+    (
+      {
+        // fname: new FormControl("",Validators.required),
+        fname: new FormControl("", Validators.compose
+        (
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(10),
+            Validators.pattern('[\\w\\-\\s\\/]+')
+          ]
+        )),
+
+        lname: new FormControl("",this.textValidator),
+        language: new FormControl("")
+      });
+  }
+
+
+  textValidator(control)
+  {
+      if(control.value.length<6)
+      {
+        return {"lname":""};
+      }
+  }
+
+  clickSubmit(userForm)
+  {
+    console.log(userForm);
+    console.log(userForm.fname);
+    console.log(userForm.lname);
+    console.log(userForm.language);
+  }
+
 }
